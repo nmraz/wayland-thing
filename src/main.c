b11303d2b8a365b7d1e6e9b3f51f8ca65ff5d9db
Noam Raz <noamraz8@gmail.com>
2025-03-29 14:53:10 +0300

    Implement connection to compositor

--- src/main.c
+++ src/main.c
@@ -1,3 +1,15 @@
+#include <stddef.h>
+#include <stdio.h>
+#include <wayland-client.h>
+
 int main(void) {
+    struct wl_display* display = wl_display_connect(NULL);
+    if (!display) {
+        puts("failed to connect");
+        return 1;
+    }
+
+    wl_display_disconnect(display);
+
     return 0;
 }
