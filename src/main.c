b4ba1307340b4cfc13773183220890d896a15729
Noam Raz <noamraz8@gmail.com>
2025-03-29 15:18:23 +0300

    Add simple registry handler

--- src/main.c
+++ src/main.c
@@ -2,6 +2,23 @@
 #include <stdio.h>
 #include <wayland-client.h>
 
+static void global_handler(void* data, struct wl_registry* registry,
+                           uint32_t name, const char* interface,
+                           uint32_t version) {
+    printf("new '%s' instance (version %u) bound at %u\n", interface, version,
+           name);
+}
+
+static void global_remove_handler(void* data, struct wl_registry* registry,
+                                  uint32_t name) {
+    printf("instance %u removed\n", name);
+}
+
+static const struct wl_registry_listener registry_listener = {
+    .global = global_handler,
+    .global_remove = global_remove_handler,
+};
+
 int main(void) {
     struct wl_display* display = wl_display_connect(NULL);
     if (!display) {
@@ -9,6 +26,18 @@ int main(void) {
         return 1;
     }
 
+    struct wl_registry* registry = wl_display_get_registry(display);
+    if (!registry) {
+        puts("failed to get registry");
+        return 1;
+    }
+
+    wl_registry_add_listener(registry, &registry_listener, NULL);
+
+    // Wait for notifications about all current globals to be handled.
+    wl_display_roundtrip(display);
+
+    wl_registry_destroy(registry);
     wl_display_disconnect(display);
 
     return 0;
